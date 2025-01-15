8b7c24e3e5581f9224a62b09fa34c7ac826e18b0
unknown <namnguyen4112k5@gmail.com>
2025-01-16 01:29:20 +0700

    create comon layout

---
+++ src/app.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import path from "path"
+import { title } from 'process';
+const app = express();
+const port = 5001
+const __dirname = path.resolve()
+
+app.use("/static", express.static(path.join(__dirname, 'public')))
+
+app.set('view engine', 'ejs')
+app.set("views", __dirname + "/src/views")
+
+app.get('/', (req, res)=>{
+    res.render("pages/index")
+})
+
+app.get('/components', (req, res)=>{
+    res.render("pages/components", {
+        title: "components"
+    })
+})
+
+app.get('/form', (req, res)=>{
+    res.render("pages/form", {
+        title: "form"
+    })
+})
+
+app.get('/icons', (req, res)=>{
+    res.render("pages/icons", {
+        title: "Icons"
+    })
+})
+
+app.get('/tables', (req, res)=>{
+    res.render("pages/tables", {
+        title: "tables"
+    })
+})
+
+app.get('/typography', (req, res)=>{
+    res.render("pages/typography", {
+        title: "typography"
+    })
+})
+
+app.get('/notifications', (req, res)=>{
+    res.render("pages/notifications", {
+        title: "notifications"
+    })
+})  
+
+app.listen(port, function() {
+    console.log("http://localhost:" + port);
+    
+})
